413c0515c4f3f3dafb00e2fda8d828a6139aee4d
ReikaAmalia <reikaamalia17>
2023-10-30 16:26:14 +0700

    update

--- praktikum 10/Number24.java
+++ praktikum 10/Number24.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Arrays;
 public class Number24 {
     public static void main(String[] args) {
         
@@ -7,5 +7,8 @@ public class Number24 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
         
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + myNumbers[1].length);
+        }
     }
 }
