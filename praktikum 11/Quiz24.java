6f4210d0bf15dd54a99f57086cbfceef7f4f7ee7
ReikaAmalia <reikaamalia17>
2023-11-06 13:52:26 +0700

    update

---
+++ praktikum 11/Quiz24.java
@@ -0,0 +1,17 @@
+public class Quiz24 {
+import java.util.Scanner;
+import java.util.Random;
+
+Random random = new Random();
+Scanner input = new Scanner(System.in);
+
+char menu='y';
+do{
+    int number = random.nextInt(10) + 1;
+    boolean success = false;
+    do{
+        System.out.print("Tebak angka (1-10): ");
+        int answer = input.nextLine()
+    }
+}
+}
