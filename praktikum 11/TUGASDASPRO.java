4b40c5795387bf522f50fdc08c26d1e7db473eff
ReikaAmalia <reikaamalia17>
2023-11-08 10:46:17 +0700

    update

--- praktikum 11/TUGASDASPRO.java
+++ praktikum 11/TUGASDASPRO.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 public class TUGASDASPRO {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan tinggi segitiga: ");
-        int tinggiSegitiga = input.nextInt();
-        
-        for (int i = tinggiSegitiga; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+        public static void main(String[] args) {
+            int tinggi = 5;
+            int lebar = 8;
+    
+            for (int i = 1; i <= tinggi; i++) {
+                for (int j = 1; j <= lebar; j++) {
+                    // Cetak '*' jika berada di pinggir atau dalam persegi
+                    if (i == 1 || i == tinggi || j == 1 || j == lebar) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-}
